95f2d84611e01eb7ef8ee8d2ae5da1f3f19a42db
Bartosz <pokji>
2024-12-28 11:34:03 +0100

    Add model.py with text normalization

--- model.py
+++ model.py
@@ -1,1 +1,32 @@
 import torch
+import pandas as pd
+import numpy as np
+
+all_slogans = pd.read_csv('all_slogans.csv', sep=';')
+slogans = all_slogans['slogan']
+slogans = slogans.str.lower()
+
+# reducing invaluable tokens
+to_remove = ['\n', '\r', '>', '\x80', '\x93', '\x94', '\x99', '\x9d', '\xa0',
+             '¦', '®', '°', 'º', '¼', '½','×', 'â', 'ã', 'è', 'é', 'ï', 'ñ', 'ú', 'ü',
+             '⁄', '（', '）', '，', '·']
+
+dict_to_remove = {"’" : "'", "‘" : "'", "“" : '"', "”" : '"',
+                  "…" : '...', '—': '-', '–': '-'}
+
+
+# normalizing the tokens
+for char in to_remove:
+    slogans = slogans.str.replace(char, ' ')
+
+
+for key, value in dict_to_remove.items():
+    slogans = slogans.str.replace(key, value)
+
+
+# getting the character set
+
+characters = [char for slogan in slogans for char in slogan]
+characters = sorted((set(characters)))
+print(characters)
+len(characters)
